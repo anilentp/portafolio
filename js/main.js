6e0ef681459af2fed37fcc82237b929356f75d6c
AniiLen <anile.tp@gmail.com>
2016-08-03 14:57:27 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$( document ).ready(function(){
+	$(".button-collapse").sideNav();
+	$('.carousel.carousel-slider').carousel({full_width: true});
+	 $('.parallax').parallax();
+/*
+	 var options = [
+    {selector: '.habilidad', offset: 200, callback: customCallbackFunc } },
+    {selector: '.habilidad', offset: 200, callback: function() {
+      customCallbackFunc();
+    } },
+  ];
+  Materialize.scrollFire(options);
+    
+
+    var options = [ 
+    {selector: '#staggered-test', offset: 50, callback: function(el) 
+    { Materialize.toast("This is our ScrollFire Demo!", 1500 ); } }, 
+    {selector: '#staggered-test', offset: 205, callback: function(el) { Materialize.toast("Please continue scrolling!", 1500 ); } }, 
+    {selector: '#staggered-test', offset: 400, callback: function(el) { Materialize.showStaggeredList($(el)); } }, 
+    {selector: '#image-test', offset: 500, callback: function(el) { Materialize.fadeInImage($(el)); } } ]; 
+    Materialize.scrollFire(options); 
+*/
+});
